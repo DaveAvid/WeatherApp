fa6b0b02d28b97994492892676df23db6de9e4a4
davea <dave.avid@gmail.com>
2021-04-13 18:21:52 -0500

    added images

--- src/main/java/com/orange/MainApplication.java
+++ src/main/java/com/orange/MainApplication.java
@@ -5,7 +5,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,14 +13,14 @@ public class MainApplication extends Application {
 
     @SuppressWarnings("restriction")
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
 
         try {
             Parent root = FXMLLoader.load(MainApplication.class.getResource("/fxml/primary.fxml"));
 
             primaryStage.setTitle("Weather");
 
-            primaryStage.getIcons().add(new Image("/images/icon.png"));
+//            primaryStage.getIcons().add(new Image("/images/icon.png"));
             primaryStage.setScene(new Scene(root, 1050, 670));
             primaryStage.getScene().getStylesheets().addAll(MainApplication.class.getResource("/styles/style.css").toExternalForm());
             primaryStage.show();
